7084a18bf4fb6ccb5249423a0637d21aee809075
csardo <csardooo@gmail.com>
2022-07-19 20:20:03 -0700

    Add button that unselects

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-var listOfBLocks = document.querySelector('#block-list');
+var listOfBLocks = document.querySelector('.container');
 
+let stack = [];
 listOfBLocks.addEventListener('click', function(event) {
-    event.target.classList.add('selected');
+    let target = event.target;
+  
+    /* Select if not yet selected */
+    if (target.className === 'box') {
+      target.classList.add('selected');
+      stack.push(target);
+    }
+  
+    /* Unselect if all in letter are selected */
+});
+
+var btn = document.querySelector('#btn');
+
+btn.addEventListener('click', function(event) {
+  for (let i = 0; i < stack.length; i++) {
+    stack[i].classList.remove('selected');
+  }
 });
